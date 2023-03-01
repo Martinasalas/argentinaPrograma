c1216f6a3551ab3571f5a290cc2e1bd72b95ec66
Martina Salas <martinasalas08@gmail.com>
2023-02-28 22:46:02 -0300

    hola

---
+++ index.js
@@ -0,0 +1,45 @@
+const dropdown = document.getElementById("datosCont")
+
+
+function myFunction(){
+    var style=dropdown.style.display;
+        if(style=='block')
+        	{
+            dropdown.style.display='none';
+        	}
+    	else{
+            dropdown.style.display='block';
+        	}    			
+
+}
+
+const dropdown2 = document.getElementById("perfProf")
+
+
+function myFunction2(){
+    var style=dropdown2.style.display;
+        if(style=='block')
+        	{
+            dropdown2.style.display='none';
+        	}
+    	else{
+            dropdown2.style.display='block';
+        	}    			
+
+}
+
+
+const dropdown3 = document.getElementById("competencias")
+
+
+function myFunction3(){
+    var style=dropdown3.style.display;
+        if(style=='block')
+        	{
+            dropdown3.style.display='none';
+        	}
+    	else{
+            dropdown3.style.display='block';
+        	}    			
+
+        }
